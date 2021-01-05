7200ea0fe947e54fffd7028dafbd122739e11aac
André Felipe Selbacch Pinto <selbacch@gmail.com>
2021-01-04 21:01:06 -0300

    quadrados 04/01/2021

---
+++ Assets/movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public bool naEscada;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && naEscada == true)
+        {
+            transform.Translate(0, 10 * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.S) && naEscada == true)
+        {
+            transform.Translate(0, -10 * Time.deltaTime, 0);
+        }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Escada")
+        {
+            naEscada = true;
+        }
+       
+    }
+
+
+
+}
+   
+
