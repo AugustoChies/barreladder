c30ace43e4baf828eef6c4e914d82a8bf4075afb
André Felipe Selbacch Pinto <selbacch@gmail.com>
2021-01-11 22:06:52 -0300

    ritimo na ui

---
+++ Assets/nota2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nota2 : MonoBehaviour
+{ 
+    public Image canvas;
+public float speed;
+private Animator CAnimation;
+private float timeDestroy;
+
+// Start is called before the first frame update
+void Start()
+{
+    CAnimation = GetComponent<Animator>();
+    CAnimation.SetBool("batida", true);
+    GameObject.Find("barrinha(Clone)").transform.parent = canvas.transform;
+
+    if (gameObject.name == "barrinha(Clone)")
+    {
+        timeDestroy = 8f;
+        Destroy(gameObject, timeDestroy);
+    }
+}
+
+void Update()
+{
+    transform.Translate(Vector2.right * speed * Time.deltaTime);
+}
+}
---
+++ Assets/play.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class play : MonoBehaviour
+{
+    public GameObject barrinha;
+public Transform point;
+   public float cronometro;
+    public float tempo;
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    cronometro += Time.deltaTime;
+
+    if (cronometro >= tempo)
+         {
+
+        GameObject CloneTiro = Instantiate(barrinha, point.position, point.rotation);
+        cronometro = 0;     
+
+         }
+
+
+    }
+}
+
+
