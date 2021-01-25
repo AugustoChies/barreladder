8fd4712c2c0317c1ecb6fd5f249fcba7ba939713
André Felipe Selbacch Pinto <selbacch@gmail.com>
2021-01-25 20:27:17 -0300

    inimigo no stage prncipal

---
+++ Assets/random.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class random : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        delete();
+
+    }
+    void delete()
+    {
+        float timeDestroy = 3f;
+        Destroy(gameObject, timeDestroy);
+
+    }
+
+
+
+}
